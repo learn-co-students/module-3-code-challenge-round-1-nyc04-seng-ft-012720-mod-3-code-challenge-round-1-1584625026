a88ad65652bd4f1797c9b2d90ebd1cfbf87d8cdb
github username <github email address>
2020-03-19 11:31:01 -0400

    Deliverables unfinished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+// Application State
+let imageId = 4864 //Enter the id from the fetched image here
 
-  let imageId = 1 //Enter the id from the fetched image here
+// DOM Elements
+const pageImage = document.querySelector('#image')
+const pageLike = document.querySelector("#likes")
+const pageComments = document.querySelector("#comments")
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+// const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
-
+// Event Listeners
+pageImage.addEventListener("click", e => {
+  // const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  
 })
+
+pageLike.addEventListener("click", e => {
+  // const likeURL = `https://randopic.herokuapp.com/likes/`
+  
+})
+
+// Render Helpers
+
+
+// Initial Fetch
+fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+  .then(r => r.json())
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+// })
