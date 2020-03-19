14cbaeecca03cb97df915058c79c958c8abdc3c3
abidtalukdar <abiditalukdar@gmail.com>
2020-03-19 11:21:26 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+// document.addEventListener('DOMContentLoaded', () => {})
+let imageId = 4871 
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+const imageCard = document.getElementById("image_card")
 
-  let imageId = 1 //Enter the id from the fetched image here
+// Step 1 - Get the Image Data
+const fetchImage = () => {
+    return fetch(imageURL)
+        .then(response => response.json())
+}
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+fetchImage()
+    .then(imageData => renderImage(imageData))
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+const renderImage = (image) => {
+    imageCard.innerHTML = `        
+    <img src="${image.url}" id="image" data-id="${image.id}"/>
+    <h4 id="name">${image.name}</h4>
+    <span>Likes:
+      <span id="likes">${image.like_count}</span>
+    </span>
+    <button id="like_button">Like</button>
+    <form id="comment_form">
+      <input id="comment_input" type="text" name="comment" placeholder="Add Comment"/>
+      <input type="submit" value="Submit"/>
+    </form>
+    <ul id="comments">
+         <!-- <li> for each comment goes here -->
+         <li>${image.comments[0].content}</li>
+    </ul>
+    `
+    // Step 2 - Like Feature (Frontend)
+    document.getElementById("like_button").addEventListener("click", renderLike)
+    // Step 4 - Comment Feature (Frontend)
+    document.getElementById("comment_form").addEventListener("submit", renderForm)
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+// Step 3 - Like Feature (Backend)
+const renderLike = (event) => {
+    const likeCount = document.getElementById("likes")    
+    likeCount.innerText = parseInt(likeCount.innerText) + 1
+    
+    fetch(likeURL, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify({image_id: 4871})
+    })
+    .then(response => response.json())
+}
 
-})
+// Step 4 - Comment Feature (Frontend)
+const renderForm = (event) => {
+    event.preventDefault()
+    const commentsUl = document.getElementById("comments")
+    
+    const commentLi = document.createElement("li")
+    commentLi.textContent = event.target["comment"].value
+    
+    commentsUl.append(commentLi)
